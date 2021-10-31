81e59ff0fb9501e0ca82fa63744f41644f63c586
Nefera606 <garciademata@gmail.com>
2021-10-31 10:41:33 +0100

    Add ubuntu set up

---
+++ installUbuntu.sh
@@ -0,0 +1,9 @@
+sudo apt upgrade
+
+sudo apt-get install lolcat
+sudo apt-get install figlet
+
+echo "Installing nvm"
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+echo "Installing jabba"
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
