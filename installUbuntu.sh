1579091d53a8c86ef2261887b8b4fcd9a45fe390
Iñigo Garcia <garciademata@gmail.com>
2021-12-07 16:56:37 +0100

    Update installUbuntu.sh

--- installUbuntu.sh
+++ installUbuntu.sh
@@ -10,3 +10,17 @@ echo "Installing nvm"
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "Installing jabba"
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+
+echo "Installing GH cli"
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+echo "installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing VIM and plugins"
+
+echo "Installing fzf"
+
+echo "Configuring environment"
